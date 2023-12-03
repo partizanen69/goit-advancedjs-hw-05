90e626c81d192172bf0c66d6c1be978e43e81e85
Oleksii Ablitsov <oleksii.ablitsov@bringg.com>
2023-12-03 14:55:07 +0200

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+set -euo pipefail
+
+npm run build
+
+rm -rf /tmp/gh-pages
+mkdir /tmp/gh-pages
+cp -r index.html ./dist /tmp/gh-pages
+cd /tmp/gh-pages
+git init
+git checkout -b gh-pages
+
+git remote add origin git@github.com:partizanen69/goit-advancedjs-hw-05.git
+
+git add -A
+git commit -m "deploy"
+git push -f --set-upstream origin gh-pages
+
+
+
+
